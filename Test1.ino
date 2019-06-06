d7ef788a43af067240eb42a3cd5f1572b3276659
sethstuart <joshuastuart24@gmail.com>
2019-06-06 01:49:02 -0700

    Add files via upload

--- Test1.ino
+++ Test1.ino
@@ -1,24 +1,79 @@
-/*
-        Arduino Brushless Motor Control
-     by Dejan, https://howtomechatronics.com
-*/
 #include <Servo.h>
-Servo ESC;     // create servo object to control the ESC
-String x;
-int y;
+
+Servo x1;
+Servo x2;
+Servo y1;
+Servo y2;
+Servo z0;
+
+const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+int x;  // forward/back axis
+int y; // left/right axis
+int z; // Up/down Axis
+
+boolean newData = false;
+boolean parsedData = false;
+
 void setup() {
-  Serial.begin(9600);
-  // Attach the ESC on pin 9
-  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
-}
-void loop() {
-  if(Serial.available()){
-    x = Serial.readString();
-    Serial.println(x);
+    Serial.begin(9600);
+    Serial.println("<Arduino is ready>");
+    x1.attach(8,1000,2000);
+    x2.attach(9,1000,2000);
+    z0.attach(10,1000,2000);
+    y1.attach(11,1000,2000);
+    y2.attach(12,1000,2000);
     
-    y = x.toInt();
-    ESC.write(y);
+}
+
+void loop() {
+    recvWithEndMarker();
+    parseRX();
+    updateXZProp();
+}
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void parseRX() {
+  if (newData == true) {
+    sscanf(receivedChars,"%*c %d %*c %d %*c %d",&x,&y,&z);
+    newData = false;
+    parsedData = true;
   }
 }
 
-
+void updateXZProp() {
+    if (parsedData == true) {
+        int xRX = x;
+        int yRX = y;
+        int zRX = z;
+        Serial.println(x);
+        Serial.println(y);
+        Serial.println(z);
+        x1.write(xRX);
+        x2.write(xRX);
+        z0.write(zRX);
+        parsedData = false;
+    }
+}
--- run.py
+++ run.py
@@ -1,26 +1,43 @@
 from flask import Flask, request, render_template
 import serial
-import time
 
 app = Flask(__name__)
 
-port = serial.Serial(port= 'COM12', baudrate=9600)
+port = serial.Serial(port= 'COM11', baudrate=9600)
 port.flushInput()
 text = b'0'
 
-@app.route('/')
-def my_form():
-	port.write(text)
-	return render_template('my-form.html')
+# @app.route('/loading')
+# def loading():
+	# rcvinit = ""
+	# initline = "<Arduino is ready>"
+	# ohgodohfuckmyairpods = 0
+	# while ohgodohfuckmyairpods == 0:
+		# rcvinit = port.readline()
+		# if rcvinit == initline:
+			# ohgodohfuckmyairpods += 1
+			# return rcvinit
+		# else:
+			# return rcvinit
 	
 	
-@app.route('/', methods=['POST'])
-def my_form_post():
-	text = request.form['text']
-	port.write(text)
-	return text
-
-time.sleep(5)
+@app.route('/write', methods=['GET','POST'])
+def writeserial():
+	if request.method == 'POST':
+		text = request.form['text']
+		text = text.encode()
+		port.write(text)
+		port.write(b'\n\r')
+		return render_template('my-form.html')
+	else:
+		# port.write(b'0\n')
+		return render_template('my-form.html')
 	
-
-
+# @app.route('/read')
+# def readserial():
+   # port.read()
+   # roombaFarts = 
+   # templateData = {
+      # 'roombafarts': roombaFarts
+      # }
+   # return render_template('readserial.html', **templateData)
